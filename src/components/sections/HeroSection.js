a00fd3c0f08f7699089da8444806fb0de097c3d1
robertoaguilar1979 <robertoaguilar1979@gmail.com>
2021-10-27 12:36:23 -0700

    updated styled-components

--- src/components/sections/HeroSection.js
+++ src/components/sections/HeroSection.js
@@ -4,23 +4,72 @@ import styled from "styled-components"
 function HeroSection() {
   return (
     <>
-      <div className="hero-wrapper">
-        <div className="hero-text">
-          <h1 className="hero-text__h1">Robert Aguilar</h1>
-          <p className="hero-text__react-shopify">React/Shopify Developer </p>
-          <p className="hero-text__mainText">
+      <HeroWrapper>
+        <HeroText>
+          <HeroTextH1>Robert Aguilar</HeroTextH1>
+          <HeroTextReactShopify>React/Shopify Developer </HeroTextReactShopify>
+          <HeroTextMainText>
             I’m a software engineer located here in the USA. I’m a Navy Veteran
             that has a passion in helping the small to medium business thrive
             online.{" "}
-          </p>
+          </HeroTextMainText>
           <a href="" className="hero-text__btn">
             Email Me
           </a>
-        </div>
+        </HeroText>
         <div className="hero-picture"></div>
-      </div>
+      </HeroWrapper>
     </>
   )
 }
 
 export default HeroSection
+
+const HeroWrapper = styled.div`
+  max-width: 1440px;
+  margin: 0 auto;
+`
+const HeroText = styled.div`
+  margin-left: 0.5em;
+`
+const HeroTextH1 = styled.h1`
+  font-family: "Playfair Display SC", serif;
+  font-size: 4rem;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 85px;
+  letter-spacing: 0.1em;
+  text-align: left;
+`
+const HeroTextReactShopify = styled.p`
+  font-family: Roboto;
+  font-size: 1.25rem;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 23px;
+  letter-spacing: 0.88em;
+  text-align: left;
+  color: #51dde8;
+
+  height: 28px;
+  max-width: 650px;
+  left: 85px;
+  top: 326px;
+  margin-bottom: 1.75em;
+  margin-left: 0.3em;
+`
+const HeroTextMainText = styled.p`
+  font-family: Poppins;
+  font-size: 1.125rem;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 27px;
+  letter-spacing: 0em;
+  text-align: left;
+
+  height: 83px;
+  max-width: 539px;
+  left: 85px;
+  top: 400px;
+  margin-left: 0.3em;
+`
---
+++ src/components/textStyles/TextStyles.js
@@ -0,0 +1,17 @@
+import styled from "styled-components"
+
+export const BodyFontSize = styled.p`
+  font-family: Poppins;
+  font-size: 2.25rem;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 61px;
+  letter-spacing: 0em;
+  text-align: left;
+`
+export const BabyBlue = styled.p`
+  background: #51dde8;
+`
+export const Green = styled.p`
+  background: #20f24e;
+`
